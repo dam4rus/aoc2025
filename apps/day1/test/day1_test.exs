03a1643a3ca93f579dab595567e0a430b7a2c68e
Róbert Kalmár <rfrostkalmar@gmail.com>
2025-12-01 10:32:09 +0100

    Finished part 1

--- apps/day1/test/day1_test.exs
+++ apps/day1/test/day1_test.exs
@@ -2,7 +2,54 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "greets the world" do
-    assert Day1.hello() == :world
+  test "rotate left" do
+    assert Day1.rotate(50, :left, 1) == 49
+  end
+
+  test "rotate left overflow" do
+    assert Day1.rotate(50, :left, 51) == 99
+  end
+
+  test "rotate left multiple overflow" do
+    assert Day1.rotate(50, :left, 201) == 49
+  end
+
+  test "rotate right" do
+    assert Day1.rotate(50, :right, 1) == 51
+  end
+
+  test "rotate right overflow" do
+    assert Day1.rotate(50, :right, 51) == 1
+  end
+
+  test "rotate right multiple overflow" do
+    assert Day1.rotate(50, :right, 201) == 51
+  end
+
+  test "single rotate line" do
+    assert Day1.process_input(["L50"]) == 1
+  end
+
+  test "multiple rotate line" do
+    assert Day1.process_input(["L51", "R1"]) == 1
+  end
+
+  test "multiple rotate line with rotation at 100" do
+    assert Day1.process_input(["L51", "R1", "R200"]) == 2
+  end
+
+  @test_input "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"
+
+  test "part one with test input" do
+    assert Day1.process_input(String.split(@test_input, "\n")) == 3
   end
 end
