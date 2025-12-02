dd0f7a2e9a1f5324b62ad8928dd79666efbfb1ec
Róbert Kalmár <rfrostkalmar@gmail.com>
2025-12-02 16:49:25 +0100

    Add comments

--- apps/day1/test/day1_test.exs
+++ apps/day1/test/day1_test.exs
@@ -2,53 +2,45 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  # test "rotate left" do
-  #   assert Day1.rotate(50, :left, 1) == {49, 0}
-  # end
+  test "rotate left" do
+    assert Day1.rotate(50, :left, 1) == {49, 0}
+  end
 
-  # test "rotate left overflow" do
-  #   assert Day1.rotate(50, :left, 51) == {99, 1}
-  # end
+  test "rotate left overflow" do
+    assert Day1.rotate(50, :left, 51) == {99, 1}
+  end
 
-  # test "rotate left multiple overflow" do
-  #   assert Day1.rotate(50, :left, 201) == {49, 2}
-  # end
+  test "rotate left multiple overflow" do
+    assert Day1.rotate(50, :left, 201) == {49, 2}
+  end
 
-  # test "rotate right" do
-  #   assert Day1.rotate(50, :right, 1) == {51, 0}
-  # end
+  test "rotate right" do
+    assert Day1.rotate(50, :right, 1) == {51, 0}
+  end
 
-  # test "rotate right overflow" do
-  #   assert Day1.rotate(50, :right, 51) == {1, 1}
-  # end
+  test "rotate right overflow" do
+    assert Day1.rotate(50, :right, 51) == {1, 1}
+  end
 
-  # test "rotate right multiple overflow" do
-  #   assert Day1.rotate(50, :right, 201) == {51, 2}
-  # end
+  test "rotate right multiple overflow" do
+    assert Day1.rotate(50, :right, 201) == {51, 2}
+  end
 
-  # test "single rotate line" do
-  #   assert Day1.process_input(["L50"], :single) == 1
-  # end
+  test "single rotate line" do
+    assert Day1.process_input(["L50"], :single) == 1
+  end
 
-  # test "multiple rotate line" do
-  #   assert Day1.process_input(["L51", "R1"], :single) == 1
-  # end
+  test "multiple rotate line" do
+    assert Day1.process_input(["L51", "R1"], :single) == 1
+  end
 
-  # test "multiple rotate line with rotation over 100" do
-  #   assert Day1.process_input(["L51", "R1", "R200"], :single) == 2
-  # end
+  test "multiple rotate line with rotation over 100" do
+    assert Day1.process_input(["L51", "R1", "R200"], :single) == 2
+  end
 
-  # test "multiple rotate line with rotation over 100 using multiple password method" do
-  #   assert Day1.process_input(["L51", "R1", "R200"], :multiple) == 4
-  # end
-
-  # test "anyad" do
-  #   assert Day1.process_input(["L50", "L200"], :multiple) == 3
-  # end
-
-  # test "anyad2" do
-  #   assert Day1.process_input(["R50", "R200", "L200"], :multiple) == 5
-  # end
+  test "multiple rotate line with rotation over 100 using multiple password method" do
+    assert Day1.process_input(["L51", "R1", "R200"], :multiple) == 4
+  end
 
   @test_input "L68
 L30
@@ -61,9 +53,9 @@ L99
 R14
 L82"
 
-  # test "part one with test input" do
-  #   assert Day1.process_input(String.split(@test_input, "\n"), :single) == 3
-  # end
+  test "part one with test input" do
+    assert Day1.process_input(String.split(@test_input, "\n"), :single) == 3
+  end
 
   test "part two with test input" do
     assert Day1.process_input(String.split(@test_input, "\n"), :multiple) == 6
--- apps/day2/lib/day2.ex
+++ apps/day2/lib/day2.ex
@@ -1,4 +1,10 @@
 defmodule Day2 do
+  @type method() :: :at_most_twice | :at_least_twice
+
+  @doc """
+  Returns whether the given input is an invalid ID with at most twice duplication.
+  """
+  @spec is_invalid?(String.t()) :: boolean()
   def is_invalid?(input) when is_binary(input) do
     with input_length <- String.length(input),
          0 <- input_length |> rem(2),
@@ -9,7 +15,12 @@ defmodule Day2 do
     end
   end
 
+  @doc """
+  Returns whether the given input is an invalid ID with at least twice duplication.
+  """
+  @spec is_invalid?(String.t(), integer()) :: boolean()
   def is_invalid?(input, chunk_size) when is_binary(input) and chunk_size > 0 do
+    # For the left-over chunk `:invalid` values are appended, so it does not equal to head
     [head | tail] =
       String.graphemes(input)
       |> Enum.chunk_every(chunk_size, chunk_size, Stream.cycle([:invalid]))
@@ -17,6 +28,15 @@ defmodule Day2 do
     tail |> Enum.all?(fn value -> value == head end)
   end
 
+  @doc """
+  Finds invalid IDs in the given range.
+
+  Range can be either a tuple of `{first, last}` or a `Range` instance.
+
+  Search method can be either `:at_most_twice` to find ids that only repeat a given pattern twice
+  or `:at_least_twice` to find ids that repeats twice or multiple times.
+  """
+  @spec invalid_ids_in_range({number(), number()} | Range.t(), method()) :: [String.t()]
   def invalid_ids_in_range(range = %Range{}, :at_most_twice) do
     range
     |> Enum.filter(fn n -> Integer.to_string(n) |> is_invalid? end)
@@ -38,6 +58,10 @@ defmodule Day2 do
     invalid_ids_in_range(Range.new(first, last), method)
   end
 
+  @doc """
+  Sum invalid ID values.
+  """
+  @spec sum_invalid_ids(String.t(), method()) :: integer()
   def sum_invalid_ids(input, method) when is_binary(input) do
     input
     |> String.split(",")
@@ -48,6 +72,7 @@ defmodule Day2 do
     |> Enum.reduce(&+/2)
   end
 
+  @spec sum_invalid_ids(method()) :: integer()
   def sum_invalid_ids(method) do
     sum_invalid_ids(Day2Input.input(), method)
   end
