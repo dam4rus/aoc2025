0a01d2bd7f8d02cff9f18a64cb1e0948d3b18ac2
Róbert Kalmár <rfrostkalmar@gmail.com>
2025-12-03 18:40:30 +0100

    Finished part 2

--- apps/day3/lib/day3.ex
+++ apps/day3/lib/day3.ex
@@ -1,4 +1,8 @@
 defmodule Day3 do
+  @doc """
+  Finds the joltage with brute force when only 2 batteries has to be turned on
+  """
+  @spec joltage(String.t()) :: integer()
   def joltage(line) when is_bitstring(line) do
     graphemes = String.graphemes(line)
 
@@ -13,9 +17,72 @@ defmodule Day3 do
     |> Enum.max()
   end
 
+  @doc """
+  Finds the joltage when 12 batteries has to be turned on.
+  """
+  @spec joltage(String.t(), integer()) :: integer()
+  def joltage(line, 12) when is_bitstring(line) do
+    graphemes = String.graphemes(line)
+    line_length = String.length(line)
+    # Define how many batteries will be not turned on.
+    skip_count = line_length - 12
+
+    initial_state = %{start_index: 0, skip_count: skip_count}
+
+    Stream.unfold(initial_state, fn %{
+                                      start_index: start_index,
+                                      skip_count: skip_count
+                                    } ->
+      case skip_count do
+        # No more batteries will be left turned off at this point. Just return each remaining battery.
+        0 ->
+          {
+            graphemes |> Enum.at(start_index),
+            %{
+              start_index: start_index + 1,
+              skip_count: 0
+            }
+          }
+
+        # Find the greatest number in a subset when there are still batteries that will be left turned off.
+        skip_count ->
+          subset =
+            graphemes
+            |> Stream.drop(start_index)
+            |> Stream.take(skip_count + 1)
+            |> Enum.to_list()
+
+          max = subset |> Enum.max()
+          index = subset |> Enum.find_index(fn n -> n == max end)
+
+          {
+            max,
+            %{
+              # The batteries before the greatest number will be never turned off.
+              start_index: start_index + index + 1,
+              # Reduce how many batteries will not be turned on by the index in the subset of batteries.
+              skip_count: skip_count - index
+            }
+          }
+      end
+    end)
+    |> Stream.take(12)
+    |> Enum.join()
+    |> String.to_integer()
+  end
+
+  @spec joltage_sum(String.t() | integer()) :: integer()
+  def joltage_sum(12), do: joltage_sum(Day3Input.input(), 12)
+
   def joltage_sum(lines) when is_bitstring(lines) do
     String.split(lines, "\n") |> Stream.map(&joltage/1) |> Enum.sum()
   end
 
+  @spec joltage_sum(String.t() | integer()) :: integer()
+  def joltage_sum(lines, 12) when is_bitstring(lines) do
+    String.split(lines, "\n") |> Stream.map(&joltage(&1, 12)) |> Enum.sum()
+  end
+
+  @spec joltage_sum() :: integer()
   def joltage_sum(), do: joltage_sum(Day3Input.input())
 end
--- apps/day3/test/day3_test.exs
+++ apps/day3/test/day3_test.exs
@@ -23,7 +23,27 @@ defmodule Day3Test do
     assert Day3.joltage("818181911112111") == 92
   end
 
+  test "returns highest joltage for first line with 12 turns" do
+    assert Day3.joltage("987654321111111", 12) == 987_654_321_111
+  end
+
+  test "returns highest joltage for second line with 12 turns" do
+    assert Day3.joltage("811111111111119", 12) == 811_111_111_119
+  end
+
+  test "returns highest joltage for third line with 12 turns" do
+    assert Day3.joltage("234234234234278", 12) == 434_234_234_278
+  end
+
+  test "returns highest joltage for fourth line with 12 turns" do
+    assert Day3.joltage("818181911112111", 12) == 888_911_112_111
+  end
+
   test "part one" do
     assert Day3.joltage_sum(input()) == 357
+  end
+
+  test "part two" do
+    assert Day3.joltage_sum(input(), 12) == 3_121_910_778_619
   end
 end
