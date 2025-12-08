150572f7131287b0ec558e97095a092dcfea3f44
Róbert Kalmár <rfrostkalmar@gmail.com>
2025-12-08 14:09:31 +0100

    Finished part 2

--- apps/day6/lib/day6.ex
+++ apps/day6/lib/day6.ex
@@ -1,12 +1,33 @@
 defmodule Day6 do
-  def sum_problems(input) when is_bitstring(input) do
-    {numbers, operations} = Day6.parse_input(input)
+  @type math_type() :: :human | :cephalopod
+
+  @doc """
+  Helper method to sum the result of all problems with either in `:human` or `:cephalodo` math type.
+
+  Returns the sum of the results of all problem.
+  """
+  @spec sum_problems(String.t(), math_type()) :: non_neg_integer()
+  def sum_problems(input, math_type) when is_bitstring(input) do
+    {numbers, operations} = Day6.parse_input(input, math_type)
 
     Stream.zip(numbers, operations)
     |> Enum.sum_by(fn {numbers, operation} -> Day6.solve(numbers, operation) end)
   end
 
-  def parse_input(input) when is_bitstring(input) do
+  @doc """
+  Parses the given input in either `:human` or `:cephalopod` format.
+
+  In `:human` format each line is read either as a table of numbers or operations.
+  Then the table of numbers are transposed and combined with their respective operations.
+
+  In `:cephalopod` format the lines are read the same way as in `:human` format.
+  Then each problems column width are calculated. Using these widths the lines are split again
+  and reduced to the same format as in `:human` format.
+
+  Returns a list of numbers for each problem and their respective operation.
+  """
+  @spec parse_input(String.t(), math_type()) :: {[[non_neg_integer()]], [String.t()]}
+  def parse_input(input, :human) when is_bitstring(input) do
     lines =
       String.split(input, "\n") |> Enum.map(fn line -> String.split(line, " ", trim: true) end)
 
@@ -20,7 +41,53 @@ defmodule Day6 do
     {numbers |> Enum.map(fn line -> Enum.map(line, &String.to_integer/1) end), operations}
   end
 
+  def parse_input(input, :cephalopod) when is_bitstring(input) do
+    lines = String.split(input, "\n")
+
+    {numbers, [operations | _]} =
+      Enum.map(lines, fn line -> String.split(line, " ", trim: true) end)
+      |> Enum.split(length(lines) - 1)
+
+    column_widths =
+      Enum.reduce(numbers, Stream.cycle([0]) |> Enum.take(length(operations)), fn line, acc ->
+        Stream.zip(line, acc)
+        |> Enum.map(fn {number, column_width} -> max(column_width, String.length(number)) end)
+      end)
+
+    numbers =
+      Stream.take(lines, length(lines) - 1)
+      |> Enum.reduce(
+        column_widths |> Enum.map(fn width -> Stream.cycle([""]) |> Enum.take(width) end),
+        fn line, acc ->
+          Stream.zip(acc, split_by_column_widths(String.graphemes(line), column_widths))
+          |> Enum.map(fn {acc_columns, problem_columns} ->
+            Stream.zip(acc_columns, problem_columns)
+            |> Enum.map(fn
+              {acc_column, " "} -> acc_column
+              {acc_column, problem_column} -> acc_column <> problem_column
+            end)
+          end)
+        end
+      )
+
+    {numbers |> Enum.map(fn line -> Enum.map(line, &String.to_integer/1) end), operations}
+  end
+
+  @spec solve(Enumerable.t(), String.t()) :: non_neg_integer()
   def solve(numbers, "+"), do: Enum.sum(numbers)
 
   def solve(numbers, "*"), do: Enum.product(numbers)
+
+  @spec split_by_column_widths([String.t()], [non_neg_integer()]) :: [String.t()]
+  defp split_by_column_widths([], []), do: []
+
+  defp split_by_column_widths(line, [width]) when is_integer(width) do
+    {column, remaining} = Enum.split(line, width)
+    [column |> Enum.take(width) | split_by_column_widths(remaining, [])]
+  end
+
+  defp split_by_column_widths(line, [width | tail]) when is_integer(width) do
+    {column, remaining} = Enum.split(line, width + 1)
+    [column |> Enum.take(width) | split_by_column_widths(remaining, tail)]
+  end
 end
--- apps/day6/test/day6_test.exs
+++ apps/day6/test/day6_test.exs
@@ -8,8 +8,13 @@ defmodule Day6Test do
 *   +   *   + "
 
   test "parses input correctly" do
-    assert Day6.parse_input(input()) ==
+    assert Day6.parse_input(input(), :human) ==
              {[[6, 45, 123], [98, 64, 328], [215, 387, 51], [314, 23, 64]], ["*", "+", "*", "+"]}
+  end
+
+  test "parses cephalopod input corrently" do
+    assert Day6.parse_input(input(), :cephalopod) ==
+             {[[1, 24, 356], [369, 248, 8], [32, 581, 175], [623, 431, 4]], ["*", "+", "*", "+"]}
   end
 
   test "solve multiply" do
@@ -21,6 +26,10 @@ defmodule Day6Test do
   end
 
   test "part one" do
-    assert Day6.sum_problems(input()) == 4_277_556
+    assert Day6.sum_problems(input(), :human) == 4_277_556
+  end
+
+  test "part two" do
+    assert Day6.sum_problems(input(), :cephalopod) == 3_263_827
   end
 end
